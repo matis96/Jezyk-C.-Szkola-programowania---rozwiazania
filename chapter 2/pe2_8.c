bc94ce1878e689df37f1b4a3116f47bd57234e84
matis96 <mateuszgrzyb96@gmail.com>
2024-07-02 19:37:47 +0200

    Create pe2_8.c

---
+++ chapter 2/pe2_8.c
@@ -0,0 +1,24 @@
+//pe2_8.c
+#include <stdio.h>
+void raz_trzy(void);
+void dwa(void);
+
+int main(void)
+{
+    printf("zaczynamy:\n");
+    raz_trzy();
+    printf("koniec!");
+    return 0;
+}
+
+void raz_trzy(void)
+{
+    printf("raz\n");
+    dwa();
+    printf("trzy\n");
+}
+
+void dwa(void)
+{
+    printf("dwa\n");
+}
