9ebec9a089f5ca7f2bdd50877a46b3e2fe66e398
matis96 <mateuszgrzyb96@gmail.com>
2024-07-02 19:33:56 +0200

    Create pe2_6.c

---
+++ chapter 2/pe2_6.c
@@ -0,0 +1,12 @@
+//pe2_6.c
+#include <stdio.h>
+
+int main(void)
+{
+    int liczba = 10;
+    int x2, kwadrat;
+    x2 = 2 * liczba;
+    kwadrat = liczba * liczba;
+    printf("liczba to %d, podwojona to %d, kwadrat tej liczby to %d", liczba, x2, kwadrat);
+    return 0;
+}
