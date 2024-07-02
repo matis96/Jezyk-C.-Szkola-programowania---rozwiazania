038af546d91d4423cbc4561049e6e4fbb11a7a7b
matis96 <mateuszgrzyb96@gmail.com>
2024-07-02 19:45:31 +0200

    Create pe3_3.c

---
+++ chapter 3/pe3_3.c
@@ -0,0 +1,8 @@
+//pe3_3.c
+#include <stdio.h>
+int main(void)
+{
+    printf("\a");
+    printf("Sally, przerazona niespodziewanym odglosem, krzyknela \"A niech mnie, co to bylo!?\"");
+    return 0;
+}
