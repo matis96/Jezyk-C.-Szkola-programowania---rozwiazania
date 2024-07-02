e00eee86cb4a4a4b57ad4a8c54b0877dc81e596b
matis96 <mateuszgrzyb96@gmail.com>
2024-07-02 19:36:13 +0200

    Create pe2_7.c

---
+++ chapter 2/pe2_7.c
@@ -0,0 +1,15 @@
+//pe2_7.c
+#include <stdio.h>
+void us(void);
+
+int main(void)
+{
+    us();us();us();printf("\n");
+    us();us();printf("\n");
+    us();
+    return 0;
+}
+void us(void)
+{
+    printf("Usmiech!");
+}
